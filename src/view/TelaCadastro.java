4ed6bc49b86cfee665f85b3430757737063be466
zzzBECK <alexandrebeck2003@gmail.com>
2023-01-11 13:23:35 -0300

    Adding input box

--- src/view/TelaCadastro.java
+++ src/view/TelaCadastro.java
@@ -1,14 +1,21 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class TelaCadastro{
     private static JFrame janela = new JFrame("Cadastro");
     private static JLabel titulo = new JLabel("Cadastro:");
+    private static JLabel nome = new JLabel("Nome:");
+    private static JTextField entradaNome = new JTextField();
     
+    private Font fonte = new TelaPrincipal().getFonte();
+
+
     public void show(){
         janela.getContentPane().setBackground(Color.darkGray);
 		janela.setResizable(false);
@@ -16,12 +23,22 @@ public class TelaCadastro{
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		janela.setLayout(null);
 
-        titulo.setFont(new TelaPrincipal().getFonte());
+        titulo.setFont(fonte);
 		titulo.setForeground(Color.white);
-        titulo.setBounds(285, 116, 345, 48);
+        titulo.setBounds(330, 121, 360, 60);
+
+        nome.setBounds(145, 218, 100, 29);
+        nome.setFont(fonte);
+
+        entradaNome.setBounds(248, 221, 350, 23);
+        entradaNome.setLayout(null);
+        entradaNome.setEnabled(false);
 
 
         janela.add(titulo);
+        janela.add(nome);
+        janela.add(entradaNome);
+
         janela.setVisible(true);
     }
 }
